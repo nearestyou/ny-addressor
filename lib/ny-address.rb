c132d781ea5d5035869dd74d6cf040f8f61273b5
Cooper <cooperhanson2020@gmail.com>
2020-06-25 13:06:35 -0500

    array input

---
+++ lib/ny-address.rb
@@ -0,0 +1,124 @@
+require 'digest'
+load 'lib/constants.rb'
+load 'lib/identifier.rb'
+
+class NYAddress
+  attr_accessor :monitor, :str, :orig, :typified, :sep, :bus, :sep_map, :sep_comma, :idr, :parts
+
+  def initialize(str)
+    @monitor = false
+    @orig = str # to keep an original
+    @str = str
+    @idr = NYIdentifier.new(self)
+    identify
+  end
+
+  def identify
+    identification = @idr.identifications
+    @sep = identification[:sep]
+    @sep_map = identification[:sep_map]
+    @sep_comma = identification[:sep_comma]
+    @bus = identification[:sep_map]
+    @locale = identification[:locale]
+    @parts = identification[:parts]
+  end
+
+  def reset_str(str = @orig)
+    @str = str
+  end
+
+  def typify
+    @typified = AddressorUtils.typify(@str)
+  end
+
+  def construct(opts = {})
+  end
+
+  def hash
+    return nil if @parsed.nil?
+    Digest::SHA256.hexdigest(construct)[0..23]
+  end
+
+  def hash99999 # for searching by missing/erroneous ZIP
+    return nil if @parsed.nil?
+    Digest::SHA256.hexdigest(construct[0..-6] + "99999")[0..23]
+  end
+
+  def unitless_hash
+    return nil if @parsed.nil?
+    Digest::SHA256.hexdigest(construct({exclude_unit: true}))[0..23]
+  end
+
+  def sns
+    @parsed ? ([@bus[:street_number] || @parsed.number || '',@parsed.street || '',@bus[:state] || @parsed.state].join('')&.downcase&.gsub('-','') || '') : ''
+  end
+
+  def eq(parsed_address, display = false)
+    return nil if @parsed.nil?
+    # for displaying errors (display ? puts(parsed_address, @parsed) : false)
+    return false if @parsed.number != parsed_address.number
+    return false if @parsed.postal_code != parsed_address.postal_code
+    return false if @parsed.street != parsed_address.street
+    return false if @parsed.unit != parsed_address.unit
+    return false if @parsed.city != parsed_address.city
+    return false if @parsed.street_type != parsed_address.street_type
+    return true
+  end
+
+  def comp(parsed_address)
+    return 0 if @parsed.nil?
+    return 0 if parsed_address.nil?
+    sims = 0
+    sims += 1 if @parsed.number == parsed_address.number
+    sims += 1 if @parsed.street == parsed_address.street
+    sims += 1 if @parsed.postal_code == parsed_address.postal_code
+    sims
+  end
+
+  def ordinalize_street(street)
+    {
+      'first' => '1st', 'second' => '2nd', 'third' => '3rd', 'fourth' => '4th', 'fifth' => '5th', 'sixth' => '6th', 'seventh' => '7th', 'eighth' => '8th', 'ninth' => '9th', 'tenth' => '10th', 'eleventh' => '11th', 'twelfth' => '12th'
+    }[street] || street
+  end
+
+  def self.string_inclusion(str1, str2, numeric_failure = false)
+    strs = [ str1.downcase.gsub(/[^a-z0-9]/, ''), str2.downcase.gsub(/[^a-z0-9]/, '') ].sort_by{|str| str.length}
+    case
+    when strs.last.include?(strs.first)
+      return 1
+    else
+      if numeric_failure
+        better_match = 0
+        short_length = strs.first.length
+        long_length = strs.last.length
+
+        (short_length - 1).downto(1) do |n|
+          0.upto(short_length - n) do |i|
+            better_match = [n, better_match].max if strs.last.include?(strs.first[i..(i+n-1)])
+          end
+        end
+
+        (long_length - 1).downto(1) do |n|
+          break if n <= better_match
+          0.upto(long_length - n) do |i|
+            better_match = [n, better_match].max if strs.first.include?(strs.last[i..(i+n-1)])
+          end
+        end
+
+        return better_match.to_f / short_length
+      else
+        return 0
+      end
+    end
+  end
+
+  def self.determine_state(state_name, zip = nil)
+    if zip
+    else
+      return US_STATES[state_name] if US_STATES[state_name]
+      return CA_PROVINCES[state_name] if CA_PROVINCES[state_name]
+      return 'ER'
+    end
+  end
+
+end
--- test/addresses.rb
+++ test/addresses.rb
@@ -4,12 +4,12 @@ load 'lib/ny-addressor.rb'
 
 class AddressesTest < MiniTest::Test
   def test_4933_yukon
-    nya = NYAddressor.new('4933 Yukon AVE N, New Hope, MN 55428')
+    nya = NYAddress.new('4933 Yukon AVE N, New Hope, MN 55428')
     assert nya.parts == {street_number: '4933', street_name: 'Yukon', street_label: 'AVE', street_direction: 'N', city: 'New Hope', state: 'MN', postal_code: '55428'}
   end
 
   def test_white_house
-    nya = NYAddressor.new('1600 Pennsylvania Ave NE, Washington, DC 20202')
+    nya = NYAddress.new('1600 Pennsylvania Ave NE, Washington, DC 20202')
     assert nya.parts == {
       street_number: '1600',
       street_name: 'Pennsylvania',
@@ -22,7 +22,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_multi_city_state
-    nya = NYAddressor.new('5301 Radford Rd, Water Town, South Dakota, USA')
+    nya = NYAddress.new('5301 Radford Rd, Water Town, South Dakota, USA')
     assert nya.parts == {
       street_number: '5301',
       street_name: 'Radford',
@@ -34,7 +34,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_rr_po_box
-    nya = NYAddressor.new('R.r. #4 Box 82, Keyser, WV 26726')
+    nya = NYAddress.new('R.r. #4 Box 82, Keyser, WV 26726')
     assert nya.parts == {
       street_name: 'R.r. #4',
       street_number: 'Box 82',
@@ -45,7 +45,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_generic_po_box
-    nya = NYAddressor.new('PO Box 1033 Los Angeles CA')
+    nya = NYAddress.new('PO Box 1033 Los Angeles CA')
     assert nya.parts == {
       street_name: 'PO Box',
       street_number: '1033',
@@ -55,7 +55,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_999_main_st_minneapolis
-    nya = NYAddressor.new('999 Main St, Minneapolis, MN, 55555')
+    nya = NYAddress.new('999 Main St, Minneapolis, MN, 55555')
     assert nya.parts[:street_number] == '999'
     assert nya.parts[:street_name] == 'Main'
     assert nya.parts[:street_label] == 'St'
@@ -65,7 +65,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_999_n_main_st_minneapolis
-    nya = NYAddressor.new('999 N Main St, Minneapolis, MN, 55555')
+    nya = NYAddress.new('999 N Main St, Minneapolis, MN, 55555')
     assert nya.parts[:street_number] == '999'
     assert nya.parts[:street_name] == 'Main'
     assert nya.parts[:street_label] == 'St'
@@ -76,7 +76,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_999_main_st_s_minneapolis
-    nya = NYAddressor.new('999 Main St S, Minneapolis, MN, 55555')
+    nya = NYAddress.new('999 Main St S, Minneapolis, MN, 55555')
     assert nya.parts[:street_number] == '999'
     assert nya.parts[:street_name] == 'Main'
     assert nya.parts[:street_label] == 'St'
@@ -87,7 +87,7 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_999_van_buren_ave_minneapolis
-    nya = NYAddressor.new('999 Van Buren Ave, Minneapolis, MN, 55555')
+    nya = NYAddress.new('999 Van Buren Ave, Minneapolis, MN, 55555')
     assert nya.parts[:street_numer] = '999'
     assert nya.parts[:street_name] = 'Van Buren'
     assert nya.parts[:street_label] = 'Ave'
@@ -97,13 +97,21 @@ class AddressesTest < MiniTest::Test
   end
 
   def test_999_main_st_south_st_paul_mn_55555
-    nya = NYAddressor.new('999 Main St, South St Paul, MN, 55555')
+    nya = NYAddress.new('999 Main St, South St Paul, MN, 55555')
     assert nya.parts[:street_number] == '999'
     assert nya.parts[:street_name] == 'Main'
     assert nya.parts[:street_label] == 'St'
     assert nya.parts[:city] == 'South St Paul'
     assert nya.parts[:state] == 'MN'
     assert nya.parts[:postal_code] == '55555'
+  end
+
+  def test_array_entry
+    nya = NYAddressor.new('4933 Yukon Ave N New Hope MN 55428')
+    assert nya.output[:street_number] == '4933'
+    nyaa = NYAddressor.new(['4933 Yukon Ave N New Hope MN 55428', '999 Main St, South St Paul, MN, 55555'])
+    assert nyaa.output[0][:street_number] == '4933'
+    assert nyaa.output[1][:street_number] == '999'
   end
 
 end
