6fd7c0311b4b170bddf103f59c18d16afd15c6d6
Cooper Hanson <cooperhanson2020@gmail.com>
2025-01-08 12:40:12 -0600

    Added tests for cross street detection

--- test/test_normalization.rb
+++ test/test_normalization.rb
@@ -9,4 +9,21 @@ class TestNormalization < Minitest::Test
   def test_missing
     assert NYAddressor::Addressor.new(nil).hash.nil?
   end
+
+  def test_remove_cross_street
+    assert_equal(
+      '1505',
+      '1505'.extend(NYAddressor::AddressHelper).remove_cross_street
+    )
+
+    assert_equal(
+      '1505',
+      '1505 & 1510'.extend(NYAddressor::AddressHelper).remove_cross_street
+    )
+
+    assert_equal(
+      '1505 Penn Ave',
+      '1505 & 1510 Penn Ave'.extend(NYAddressor::AddressHelper).remove_cross_street
+    )
+  end
 end
