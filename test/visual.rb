83bed02c35d2ba9e1cfb81e122e1bd0fbceee185
Cooper Hanson <cooperhanson2020@gmail.com>
2025-01-20 11:51:34 -0600

    Readded the visual tester

--- test/visual.rb
+++ test/visual.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
-
-# gem build ny-addressor.gemspec
-# bundle info ny-addressor
-
-ENV['LOCAL_DEPENDENCIES'] = 'true'
-load 'lib/ny-addressor.rb'
+require_relative '../lib/ny-addressor'
 
 inp = '&#34;N72 W13400 LUND LN SUITE&#34;,MENOMONEE FALLS,WI,53051'
 inp = '26059 MISSION BLVD.,HAYWARD,CA,94544'
@@ -12,10 +7,11 @@ inp = '14351 104 Ave, Surrey, BC V3T 1Y1, Canada'
 inp = '1310 Ashford Ave San Juan PR 907'
 inp = 'Carretera Estatal 115, Km. 26.9 Bo. Tablonal AGUADA PR 00602'
 inp = 'E-9 Av. Luis Muñoz Marín, Caguas, 00725, Puerto Rico'
+inp = '161 Victoria St N, Saint Paul, MN 55104, United States'
+# inp = '1600 North Penn Ave, Washington, DC, 20500, United States'
 
-nya = NYAddressor.new(inp)
-nya.sep_map.each { |sep| puts sep }
-puts nya.to_s
-puts 'hash requires street name, number, & state'
-# puts nya.hash, nya.unitless_hash, nya.hash99999
-# puts nya.sns, nya.construct, nya.parts
+nya = NYAddressor::Addressor.new(inp)
+puts nya.debug
+puts nya.sns
+puts nya.construct
+puts nya.hash
