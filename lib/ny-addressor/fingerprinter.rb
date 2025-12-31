632463369342a0ea519f6008c3f4acb3a557d8a6
Gabe Carlson <xGabeC@gmail.com>
2025-12-31 16:08:44 -0600

    Switched Nil check to only fail if no required field present

--- lib/ny-addressor/fingerprinter.rb
+++ lib/ny-addressor/fingerprinter.rb
@@ -22,13 +22,13 @@ module NYAddressor
     # @return [String,nil]
     def construct(parts, opts = {})
       required = {
-        house_number: NYAddressor.first_present(parts[:house_number], parts[:house], parts[:street_name]),
-        street_name: NYAddressor.first_present(parts[:street_name], parts[:suburb], parts[:house], parts[:house_number]),
-        city: NYAddressor.first_present(parts[:city], parts[:city_district], parts[:suburb], parts[:state], parts[:postcode], parts[:country]) #Country here might break countryless fingerprints and postcode
+        house_number: NYAddressor.first_present(parts[:house_number], parts[:house], parts[:po_box], parts[:street_name]),
+        street_name: NYAddressor.first_present(parts[:street_name], parts[:suburb]),
+        city: NYAddressor.first_present(parts[:city], parts[:city_district], parts[:state], parts[:postcode], parts[:country]) #Country here might break countryless fingerprints and postcode
       }
 
-      # TODO: Asl Cooper thoughts on now that we have first_present, can we switch this to just check if any are nil/empty?
-      return nil if required.values.any? { |v| v.to_s.empty? }
+      # TODO: Asl Cooper thoughts on now that we have first_present, can we switch this to just check if any are nil/empty? change to .all?
+      return nil if required.values.all? { |v| v.to_s.empty? }
 
       opts = {
         include_label: false,
